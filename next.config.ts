52017ca76bf4bf03ce805bb560ad91ed3bd13281
Kevin Wiratama Sangehao Hia <kevin063@gmail.com>
2025-10-21 16:59:29 +0700

    revision

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '5mb', // Increase limit (e.g., 10MB)
+    },
+  },
 };
 
 export default nextConfig;
